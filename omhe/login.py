fca442c63a64af82eb3f55c45841915b063ec516
Alan <alan@ada.(none)>
2010-07-13 19:28:56 -0400

    now uses for pycurl instead or urllib/2

--- omhe/login.py
+++ omhe/login.py
@@ -1,73 +1,23 @@
 import urllib2, urllib, sys, base64
-import types, time
+import types, time, pycurl
 
 
+
+theurl = 'http://127.0.0.1:8000/api/testlogin/'
+protocol = 'http://'
+username = 'alan'
+password = 'pass'         # a great password
+    
 
 def login():
-    try:
-        theurl="http://127.0.0.1:8000/api/testlogin/"
-        password="pass"
-        username="alan"
-    
-        values= {
-                'txidr': "1",
-                'note': "The note",
-                }
-        
-        data =urllib.urlencode(values)
-        base64string = base64.encodestring('%s:%s' % (username, password))[:-1]
-        authheader =  "Basic %s" % base64string
-        req=urllib2.Request(theurl, data)
-        req.add_header("Authorization", authheader)
-        response = urllib2.urlopen(req)
-        if response.code==200:
-            time.sleep(2)
-            reply = response.read()
-            print reply
-        else:
-            print response.code
-            
-    except urllib2.HTTPError, exc:
-        if exc.code==401:
-            print "You are not authorized."
-        elif exc.code==400:
-            raise InvalidTemplateError, "Bad Request."
-        else:
-            raise UnknownHTTPError, "An unknown  urllib2.HTTPError occured."
-    except:
-        print sys.exc_info()
-        
-        
-def login2():
-    theurl = 'http://127.0.0.1:8000/api/testlogin/'
-    protocol = 'http://'
-    username = 'alan'
-    password = 'pass'         # a great password
-
-    passman = urllib2.HTTPPasswordMgrWithDefaultRealm()      # this creates a password manager
-    passman.add_password(None, theurl, username, password)      # because we have put None at the start it will always use this username/password combination
-
-    authhandler = urllib2.HTTPBasicAuthHandler(passman)                 # create the AuthHandler
-
-    opener = urllib2.build_opener(authhandler)                                  # build an 'opener' using the handler we've created
-    # you can use the opener directly to open URLs
-    # *or* you can install it as the default opener so that all calls to urllib2.urlopen use this opener
-    urllib2.install_opener(opener)
-    post_dict={'hi':'ho','howdy':'do'}
-    
-    data =urllib.urlencode(post_dict)
-        
-    req=urllib2.Request(theurl, data)
-    response = urllib2.urlopen(req)    
-    status = response.code
-    print "status=%s" % (status)
-    the_page = response.read()        
-    print the_page
-        
+    user_and_pass="%s:%s" % (username, password)    
+    c = pycurl.Curl()
+    c.setopt(pycurl.URL, theurl)
+    c.setopt(pycurl.HTTPHEADER, ["Accept:"])
+    c.setopt(pycurl.USERPWD, user_and_pass)
+    c.perform()
+    print ""
 
 if __name__ == "__main__":
     login()
-    login()
-    login()
-    login()
-    #login2()    
+    
---
+++ omhe/upload2restcat.py
@@ -0,0 +1,76 @@
+import pycurl
+import sys
+import parseomhe
+
+
+USERNAME='alan'
+PASSWORD='pass'
+URL="http://127.0.0.1:8000/api/create/"
+routing={
+        '_sndr':"alan",
+        '_rcvr':"alan",
+        '_subj':"bill",
+         '_sec':"3",
+        }
+
+def upload_OMHE_2_RESTCat(omhe_dict, outfile):
+    f = open(outfile, "wb")
+    """Send an HTTP POST to RESTCat using a simple OMHE String"""
+    pf=[]
+    post_dict={}
+    post_dict['_ttype']='omhe'
+    post_dict.update(routing)
+    post_dict['_tx_dt']=omhe_dict['_tx_dt']
+    post_dict['_tx_tz']=omhe_dict['_tx_tz']
+    post_dict['_id']=omhe_dict['_id']
+    post_dict['_texti']=omhe_dict['_texti']
+    
+    for o in post_dict:
+        x=(str(o), str(post_dict[o]))
+        pf.append(x)
+    print pf
+
+    user_and_pass="%s:%s" % (USERNAME, PASSWORD)    
+    c = pycurl.Curl()
+    c.setopt(pycurl.URL, URL)
+    c.setopt(c.HTTPPOST, pf)
+    c.setopt(c.WRITEDATA, f) 
+    c.setopt(pycurl.HTTPHEADER, ["Accept:"])
+    c.setopt(pycurl.USERPWD, user_and_pass)
+    c.perform()
+    f.close() 
+    return c
+    
+
+if __name__ == "__main__":
+    """
+    Accept a singe omhe string from the command line. Parse, then print
+    the resulting dict, then upload to RESTCat
+    """
+    try: 
+        omhe_str=sys.argv[1]
+    except(IndexError):
+        print "You must supply an omhe message!"
+        exit(1)
+        
+    try: 
+        out_file=sys.argv[2]
+    except(IndexError):
+        print "You must an output file!"
+        exit(1)
+    
+    print "Input omhe string is: %s" % (omhe_str) 
+    
+    try:
+        """ Instantaiate an instance of the OMHE class"""
+        o = parseomhe.OMHE()
+        """Parse it if valid, otherwise raise the appropriate  error"""
+        d=o.parse(omhe_str)
+        """Send the OMHE dictonary to RESTCat"""
+        result=upload_OMHE_2_RESTCat(d, out_file)
+        print "HTTP Response Code=%s" % (result.getinfo(result.HTTP_CODE),)
+        result.close()
+        
+    except():
+        print "An unexpected error occured. Here is the post-mortem:"
+        print sys.exc_info()
