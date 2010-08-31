2ab4351f07cd9d8ddf1717fc528ab10661129179
Alan <alan@ada.(none)>
2010-08-31 17:17:51 -0400

    added status bar and command line args.

--- omhe/bloodpressure.py
+++ omhe/bloodpressure.py
@@ -6,7 +6,55 @@
 import sys
 from Tkinter import *
 import tkMessageBox
-from settings import USERNAME, PASSWORD
+from settings import USERNAME, PASSWORD, RECEIVER, SENDER, RESTCAT_SERVER, SERIAL_PORT
+import parseomhe
+import pycurl
+
+bp_value=""
+username=""
+subject=""
+password=""
+syst=""
+dias=""
+puls=""
+
+def uploadOMHE2restcat(omhe_dict, username, password, sndr, rcvr, subj, security=3):
+
+        URL="%s/api/create/" % (RESTCAT_SERVER)
+        URL=str(URL)
+        user_and_pass="%s:%s" % (username, password)
+        user_and_pass=str(user_and_pass)
+        pf=[]
+        routing={'sndr':sndr,
+            'rcvr':RECEIVER,
+            'subj':sndr,
+             'sec':security,}
+        post_dict={}
+        post_dict['ttype']='omhe'
+        post_dict.update(routing)
+        post_dict['tx_dt']=omhe_dict['tx_dt']
+        post_dict['tx_tz']=omhe_dict['tx_tz']
+        post_dict['id']=omhe_dict['id']
+        post_dict['texti']=omhe_dict['texti']
+        
+        print post_dict
+        
+        for o in post_dict:
+            x=(str(o), str(post_dict[o]))
+            pf.append(x)   
+        
+        
+        c = pycurl.Curl()
+        c.setopt(pycurl.URL, URL)
+        c.setopt(c.HTTPPOST, pf)
+        c.setopt(pycurl.FOLLOWLOCATION, 1)
+        c.setopt(pycurl.MAXREDIRS, 5)
+        c.setopt(pycurl.HTTPHEADER, ["Accept:"])
+        c.setopt(pycurl.USERPWD, user_and_pass)
+        c.perform()
+        return c
+
+
 
 class App:
     """ A blood pressure GUI application based on OMHE Microsyntax"""
@@ -33,7 +81,7 @@ class App:
         self.systolicsb = Spinbox(frame, from_=0.0, to=300.0, increment=1)
         self.systolicsb.grid(row=1, column=2)
         self.systolicsb.delete(0)
-        self.systolicsb.insert(ANCHOR,"120")
+        self.systolicsb.insert(ANCHOR,syst)
         
          
         l2 = Label(frame, text="Diastolic:")
@@ -41,14 +89,31 @@ class App:
         self.diastolicsb = Spinbox(frame, from_=0.0, to=300.0, increment=1)
         self.diastolicsb.grid(row=2, column=2)
         self.diastolicsb.delete(0)
-        self.diastolicsb.insert(ANCHOR,"85")
+        self.diastolicsb.insert(ANCHOR,dias)
         
         l3 = Label(frame, text="Pulse:")
         l3.grid(row=3, column=1)
         self.pulsesb = Spinbox(frame, from_=0.0, to=300.0, increment=1)
         self.pulsesb.grid(row=3, column=2)
         self.pulsesb.delete(0)
-        self.pulsesb.insert(ANCHOR,"60")
+        self.pulsesb.insert(ANCHOR,puls)
+        
+        self.username = Label(frame, text="Username:")
+        self.username.grid(row=4, column=1)
+        self.e2 = Entry(frame)
+        self.e2.grid(row=4, column=2)
+        
+        self.username = Label(frame, text="Subect's Email:")
+        self.username.grid(row=5, column=1)
+        self.e3 = Entry(frame)
+        self.e3.grid(row=5, column=2)
+        
+        self.username = Label(frame, text="Password:")
+        self.username.grid(row=6, column=1)
+        self.e4 = Entry(frame)
+        self.e4.grid(row=6, column=2)
+
+
 
         
         try:
@@ -71,14 +136,6 @@ class App:
         self.button = Button(frame, text="          SEND             ", command=self.sendVidentity, state=mystate)
         self.button.grid(row=2, column=5)
         
-        #try:
-        #    import twitter
-        #    mystate=ACTIVE
-        #except(ImportError):
-        #    mystate=DISABLED
-        #    print "The Twitter package was not found. Disabling Twitter Send."
-        #self.button = Button(frame, text=" SEND VIA TWITTER  ", command=self.sendTwitter, state=mystate)
-        #self.button.grid(row=3, column=5)
         
         self.button = Button(master, text="RESET", fg="red", command=self.reset)
         self.button.grid(row=5, column=2)
@@ -86,29 +143,57 @@ class App:
         self.button = Button(master, text="QUIT", fg="red", command=frame.quit)
         self.button.grid(row=5, column=3)
         
+        
+        self.stat_str = StringVar()
+        self.stat_str.set("Press Send when ready")
+        self.status = Label(master, textvariable=self.stat_str, fg="red")
+        self.status.grid(row=10, column=0)
+        
+        
         self.firstdot=True
+        
+        #if bp_value!="":
+        #    self.e1.delete(0, END)
+        #    self.e1.insert(END, bp_value)
+        #
+            
+        if username!="":
+            self.e2.delete(0, END)
+            self.e2.insert(END, username)
+
+            
+        if subject!="":
+            self.e3.delete(0, END)
+            self.e3.insert(END, subject)
+
+            
+        if password!="":
+            self.e4.delete(0, END)
+            self.e4.insert(END, password)
+        
       
     def reset(self):
         self.systolicsb.delete(0,END)
-        self.systolicsb.insert(ANCHOR,"120")
+        self.systolicsb.insert(ANCHOR,syst)
         self.diastolicsb.delete(0,END)
-        self.diastolicsb.insert(ANCHOR,"85")
+        self.diastolicsb.insert(ANCHOR,dias)
         self.pulsesb.delete(0,END)
-        self.pulsesb.insert(ANCHOR,"60")
+        self.pulsesb.insert(ANCHOR,puls)
       
     
       
     def getFromMeter(self):
-        print "get from meter"
+        self.stat_str.set("Press Start when Ready.")
         import serial
-        ser = serial.Serial(serial_port, xonxoff=1)
+        ser = serial.Serial(SERIAL_PORT, xonxoff=1)
         """pop up a window and tell the person to press start on the device"""
         print ser
         #tkMessageBox.showinfo("Grab your Blood Pressure",
         #                      "Place the cuff around your arm, and press START on the meter.")
+        self.stat_str.set("Getting reading.  Please wait...")
         print "waiting for device reading..."
         s = ser.read(10)
-        print "Complete!"
+        self.stat_str.set("Done Getting BP.  Press Send to Upload")
         print s
         sys=s[2:4]
         dia=s[4:6]
@@ -132,59 +217,69 @@ class App:
         
         
     def sendVidentity(self):
-        import parseomhe
-        import upload2restcat
+        self.stat_str.set("Uploading...")
+        print "Upload to RESTCat"
         
-        print "Send via Videntity"
-        
-        omhe_str="%s%s%s%s%s%s" %(self.omhe_bp_prefix, self.systolicsb.get(),
+        self.omhe_str="%s%s%s%s%s%s" %(self.omhe_bp_prefix, self.systolicsb.get(),
                               self.omhe_s_d_deliniator, self.diastolicsb.get(),
                               self.pulse_deliniator,
                               self.pulsesb.get())
-        print omhe_str
-        """ Instantaiate an instance of the OMHE class"""
+        user=(self.e2.get())
+        email=(self.e3.get())
+        password=(self.e4.get())
+        print "user=%s" % (user)
+        print "email=%s" % (email)
+        print "password=%s" % (password)
+
+        
+        omhe_str=self.omhe_str
+        """ Instantiate an instance of the OMHE class"""
         o = parseomhe.OMHE()
-        """Parse it if valid, otherwise raise the appropriate  error"""
-        d=o.parse(omhe_str)
-        """Send the OMHE dictonary to RESTCat"""
-        result=upload2restcat.upload_OMHE_2_RESTCat(d, "out.txt", USERNAME, PASSWORD)
-        response_code= str(result.getinfo(result.HTTP_CODE, ))
-        print "HTTP Response Code=%s" % (response_code)
-        if response_code=="200":
-            exit(0)    
+        """Parse it if valid, otherwise raise the appropriate error"""
+        d=o.parse(self.omhe_str)
 
+        result=uploadOMHE2restcat(d, user, password, email, RECEIVER,
+                                  email, 2)
+        print "HTTP Response Code=%s" % (result.getinfo(result.HTTP_CODE),)
+        code = result.getinfo(result.HTTP_CODE)
+        if int(code)==200:
+            self.stat_str.set("Done! Peace out!")
+            exit(0)
+        else:
+            s="Error Uploading. Error code %s" % (code)
+            print s
+            self.stat_str.set(s)
+            
+  
 
-        
-        
-        
-    #def sendTwitter(self):
-    #    print "Send via Twitter"
-    #    #self.progressbar.set(value=25.0)
-    #    
-    #    dm = "%s%s%s%s%s%s" %(self.omhe_bp_prefix, self.systolicsb.get(),
-    #                          self.omhe_s_d_deliniator, self.diastolicsb.get(),
-    #                          self.pulse_deliniator,
-    #                          self.pulsesb.get())
-    #    print dm
-    #    try:
-    #        import twitter
-    #        api = twitter.Api(username=twitterid, password=twitterpass)
-    #        print api
-    #        result = api.PostDirectMessage(twitterreceiver, dm)
-    #        if result:
-    #            print "Successfuly sent DM"
-    #            if public_tweet==True:
-    #                try:
-    #                    api.PostUpdate(dm)
-    #                except:
-    #                    print "Failed to send Tweet"
-    #                
-    #            self.reset()
-    #        else:
-    #            print "There was a problem sending your DM tweet.  Please check user, pass and that the reciever is following you."
-    #    except:
-    #        print "There was a problem sending your DM tweet. Please check user, pass and that the reciever is following you."
-    #
+try:
+    bp_value = sys.argv[1]
+    syst=int(bp_value[0:3])
+    dias=int(bp_value[3:6])
+    puls=int(bp_value[6:9])
+    
+except:
+
+    bp_value=""
+    syst="120"
+    dias="80"
+    puls="60"
+    
+try:
+    username = sys.argv[2]
+except:
+    username ="" 
+    
+try:
+    subject = sys.argv[3]
+except:
+    subject =""
+    
+try:
+    password = sys.argv[4]
+except:
+    password ="" 
+
 
     
 root = Tk()
