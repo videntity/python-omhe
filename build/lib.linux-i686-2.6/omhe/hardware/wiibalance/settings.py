d71c29deab35871debc45d10555b9d80dbf0a02c
Alan Viars <alan@alan-laptop.(none)>
2010-10-29 10:18:25 -0400

    removing build dir

--- build/lib.linux-i686-2.6/omhe/hardware/wiibalance/settings.py
+++
@@ -1,6 +0,0 @@
-#settings.py
-callback_url="http://127.0.0.1:80/callback/wiibalance"
-server_ip="0.0.0.0"
-server_port=8002
-#weight output file
-weight_output_file="./wiiweightout.txt"
--- build/lib.linux-i686-2.6/omhe/models.py
+++
@@ -1,2 +0,0 @@
-# This file 'models.py' is here to make this a valid Django application and 
-# can remain blank.
