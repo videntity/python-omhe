a67b8d0da75eb58d5230d429e4b0ddeaa2716b70
aviars <aviars@videntity.com>
2011-01-03 19:24:04 -0500

    Fixed bug and built tests for fatmass validator

---
+++ omhe/tests/fm_test.py
@@ -0,0 +1,39 @@
+from omhe.tests.OMHETestCase import OMHETestCase
+import unittest
+from omhe.core.parseomhe import parseomhe
+from omhe.validators.validator_errors import *
+
+class fm_test(OMHETestCase):
+    validValues = ('fm30', 'fm100', 'fatmass=60#tag','fatmass=60',
+                   'fatmass=30', 'fm=22')
+    invalidOutOfRangeValues = ('fm0',)
+    invalidCommand = ('foo120/80p60#eee', 'bar=120',) 
+
+    valid_parse_val_1="fm_numeric"
+
+    def testValidValuesAlwayscontainsFatMassNumericValue(self):
+        """parse() of validValues should always return fm_mumeric in dict."""
+        for i in self.validValues:
+            p=parseomhe()
+            result = p.parse(i)
+            self.assertDictContains(result, self.valid_parse_val_1)
+            
+            
+    def testInvalidOutOfRangeValues(self):
+        """validate() of invalideOutOfRangevlues should always raise InvalidValueError."""
+        for i in self.invalidOutOfRangeValues:
+            p=parseomhe()
+            d=p.split(i)
+            self.assertRaises(InvalidValueError, p.validate, splitdict=d)
+    
+    def testInvalidMessageRaisesInvalidMessageError(self):
+        """split() of invalidCommand should always raise InvalidMessageError."""
+        for i in self.invalidCommand:
+            p=parseomhe()
+            
+            
+            self.assertRaises(InvalidMessageError, p.split, message=i)
+            
+            
+if __name__ == "__main__":
+    unittest.main()
--- omhe/validators/fm_validator.py
+++ omhe/validators/fm_validator.py
@@ -7,34 +7,24 @@ from omhe.validators.utils import *
 
 def fm_validator(omhe_value):
     valdict={}
-    
-    if omhe_value.isdigit():
+    if omhe_value.endswith('l') or omhe_value.endswith('k'):
+        valdict['fm_numeric']=omhe_value[:-1]
+        valdict['fm_measure_unit']=omhe_value[-1]
+    else:
         valdict['fm_numeric']=omhe_value
         valdict['fm_measure_unit']="l"
-        try:
-            x=float(valdict['fm_numeric'])
-            return valdict
-        except:
-            raise InvalidValueError("You didn't supply a numerical fat mass. ")
-            
-    if omhe_value.endswith('l'):
-        valdict['fm_numeric']=omhe_value[:-1]
-        valdict['fm_measure_unit']="l"
-        try:
-            x=float(valdict['fm_numeric'])
-            return valdict
-        except:
-            raise InvalidValueError("You didn't supply a numerical fat mass. ")
+        
+    try:
+        f=float(valdict['fm_numeric'])
+        if f < 1.0:
+            error_msg="Fat mass may not be less than 1."
+            raise InvalidValueError(error_msg)
+        
+    except ValueError:
+        error_msg="I could not validate the value %s is not a numerical fat mass. " % (valdict['fm_numeric'])
+        raise InvalidMessageError(error_msg)
+    
+    
         
     
-    if omhe_value.endswith('k'):
-        valdict['fm_numeric']=omhe_value[:-1]
-        valdict['fm_measure_unit']="k"
-        try:
-            x=float(valdict['fm_numeric'])
-            return valdict 
-        except:
-            raise InvalidValueError("You didn't supply a numerical fat mass. ")
-    
-    error_msg="I could not validate the value %s" % (omhe_value)
-    raise InvalidMessageError(error_msg)
+    return valdict
