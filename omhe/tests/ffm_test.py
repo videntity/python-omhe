65e88513b87fb3efee08cac2f2b045bef4881f69
Mark Scrimshire <mscrimshire@gmail.com>
2012-04-01 14:10:13 -0400

    tests for fm and ffm

--- omhe/tests/ffm_test.py
+++ omhe/tests/ffm_test.py
@@ -1,6 +1,5 @@
 __author__ = 'mark [AT] ekivemark [dot] com'
 
-
 from omhe.tests.OMHETestCase import OMHETestCase
 import unittest
 from omhe.core.parseomhe import parseomhe
@@ -10,33 +9,32 @@ from test_utils import *
 TESTS_DEBUG = True
 
 """
-Command=bg
-Name=Blood Glucose Level
-Alias=bloodglucose
-Status=BETA
-Options=Only # tags are allowed.
-Notes=Used to express a blood glucose level.
-Range=0-500
-Examples=bg120, bg135, bg=124, bg200#off the charts today
+_ffm_
+*Free Fat Mass*
+freefatmass
+BETA
+'k' suffix indicates kilograms. 'l' suffix indicates lbs. lbs is assumed if no weight provided.
+# tags are allowed	Used to express a body's free fat mass.
+ffm12 ffm=20, ffm=20k, freefatmass=20l#foo
 
 """
 
 
-class bg_test(OMHETestCase):
-    validValues = ('bg120', 'bg100#note', 'bg=399#Almost at limit','bg=80',
-                   'bloodglucose=140', 'bloodglucose200')
-    invalidOutOfRangeValues = ('bg501','bg=-01','bg=501','bloodglucose=-1','bloodglucose-1','bg-1','bg=-10')
-    invalidCommand = ('foo120/80p60', 'bar=120000p60','blood=20')
+class fm_test(OMHETestCase):
+    validValues = ('ffm30', 'ffm100', 'freefatmass=60#tag','freefatmass=60','freefatmass30',
+                   'freefatmass=30', 'ffm=22', 'ffm30l', 'ffm=30k', 'ffm=30k#tag', 'ffm=30l#tag',
+                   'freefatmass=0','freefatmass0')
+    invalidOutOfRangeValues = ('ffm#message','ffm')
+    invalidCommand = ('foo120/80p60#eee', 'bar=120','freefat=100')
 
-    valid_parse_val_1="bg"
-
+    valid_parse_val_1="ffm_numeric"
 
     if TESTS_DEBUG==True:
-        print "================== START of BG TEST ================"
+        print "================== START of FFM TEST ================"
 
-    def test_ValidValues_AlwaysContains_bg_Value(self):
-        """parse() of validValues should always return bg in dict."""
 
+    def test_ValidValues_AlwaysContains_FreeFatMass_NumericValue(self):
+        """parse() of validValues should always return fm_numeric in dict."""
         if TESTS_DEBUG==True:
             display_function_info()
             display_data_set("Valid Values Set:",self.validValues)
@@ -44,12 +42,14 @@ class bg_test(OMHETestCase):
         for i in self.validValues:
             p=parseomhe()
             result = p.parse(i)
+
             if TESTS_DEBUG==True:
                 display_test_result(i,result)
 
             self.assertDictContains(result, self.valid_parse_val_1)
             
-    def test_InvalidOutOfRangeValues(self):
+            
+    def test_Invalid_OutOfRangeValues(self):
         """validate() of invalidOutOfRangeValues should always raise InvalidValueError."""
 
         if TESTS_DEBUG==True:
@@ -73,12 +73,13 @@ class bg_test(OMHETestCase):
 
             self.assertRaises(InvalidValueError, p.validate, splitdict=d)
     
-    def testInvalidMessageRaisesInvalidMessageError(self):
+    def test_InvalidMessage_Raises_InvalidMessageError(self):
         """split() of invalidCommand should always raise InvalidMessageError."""
 
         if TESTS_DEBUG==True:
             display_function_info()
             display_data_set("Invalid Command Set:",self.invalidCommand)
+
 
         for i in self.invalidCommand:
             p=parseomhe()
--- omhe/tests/fm_test.py
+++ omhe/tests/fm_test.py
@@ -1,7 +1,24 @@
+__author__ = 'mark [AT] ekivemark [dot] com'
+
 from omhe.tests.OMHETestCase import OMHETestCase
 import unittest
 from omhe.core.parseomhe import parseomhe
 from omhe.core.validators.validator_errors import *
+from test_utils import *
+
+TESTS_DEBUG = True
+
+"""
+_fm_
+*Fat Mass*
+fatmass
+BETA
+'k' suffix indicates kilograms. 'l' suffix indicates lbs. lbs is assumed if no weight provided.
+# tags are allowed	Used to express a body fat mass.
+fm12 fm=20, fm=20k, fatmass=20l#foo
+
+"""
+
 
 class fm_test(OMHETestCase):
     validValues = ('fm30', 'fm100', 'fatmass=60#tag','fatmass=60',
@@ -11,27 +28,63 @@ class fm_test(OMHETestCase):
 
     valid_parse_val_1="fm_numeric"
 
-    def testValidValuesAlwaysContainsFatMassNumericValue(self):
+    if TESTS_DEBUG==True:
+        print "================== START of FM TEST ================"
+
+
+    def test_ValidValues_AlwaysContains_FatMass_NumericValue(self):
         """parse() of validValues should always return fm_numeric in dict."""
+        if TESTS_DEBUG==True:
+            display_function_info()
+            display_data_set("Valid Values Set:",self.validValues)
+
         for i in self.validValues:
             p=parseomhe()
             result = p.parse(i)
+
+            if TESTS_DEBUG==True:
+                display_test_result(i,result)
+
             self.assertDictContains(result, self.valid_parse_val_1)
             
             
-    def testInvalidOutOfRangeValues(self):
+    def test_Invalid_OutOfRangeValues(self):
         """validate() of invalidOutOfRangeValues should always raise InvalidValueError."""
+
+        if TESTS_DEBUG==True:
+            display_function_info()
+            display_data_set("Invalid Out of Range Values Set:",self.invalidOutOfRangeValues)
+
+
         for i in self.invalidOutOfRangeValues:
             p=parseomhe()
+            if TESTS_DEBUG==True:
+                print "parseomhe result:"
+                print p
+                print p.validate
+
             d=p.split(i)
+            if TESTS_DEBUG==True:
+                print "output of split: [" + str(d) +"]"
+
+            if TESTS_DEBUG==True:
+                display_test_result(i,d)
+
             self.assertRaises(InvalidValueError, p.validate, splitdict=d)
     
-    def testInvalidMessageRaisesInvalidMessageError(self):
+    def test_InvalidMessage_Raises_InvalidMessageError(self):
         """split() of invalidCommand should always raise InvalidMessageError."""
+
+        if TESTS_DEBUG==True:
+            display_function_info()
+            display_data_set("Invalid Command Set:",self.invalidCommand)
+
+
         for i in self.invalidCommand:
             p=parseomhe()
-            
-            
+            if TESTS_DEBUG==True:
+                display_test_result(i,p)
+
             self.assertRaises(InvalidMessageError, p.split, message=i)
             
             
